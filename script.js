7a29f1bba4880d173ff471be8d48a8688a3543d4
erickarielrodriguez <erickrodriguez584123@gmail.com>
2023-01-19 20:09:21 -0300

    004

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const button =document.querySelector('header button')
 
-const data = {
-  run: ["01-01", "01-02", "01-06"],
-  takePills: ["01-03", "01-05", "01-06", "01-07"],
-  journal: ["01-02", "01-07"],
+button.addEventListener('click',add)
+form.addEventListener('change',save)
+
+function add(){
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  //const today= '24/20'
+  const dayExists = nlwSetup.dayExists(today)
+  if(dayExists){
+    alert("Dia ja incluido 🛑")
+    return
+  }
+
+  alert('Adicionado com sucesso ✅')
+  nlwSetup.addDay(today)
 }
+
+function save(){
+  localStorage.setItem('@habits', JSON.stringify(nlwSetup.data))
+}
+const data = JSON.parse(localStorage.getItem("@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
